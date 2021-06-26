5edf659c2354d1de4e43de5981557cea417c48c0
kevnefeg <00005720@uca.edu.sv>
2021-06-26 09:01:25 -0600

    2 parte inter

--- Finalproject/View/FormNewStaff.Designer.cs
+++ Finalproject/View/FormNewStaff.Designer.cs
@@ -30,6 +30,7 @@ namespace Finalproject
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_NewStaff));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.lblid = new System.Windows.Forms.Label();
             this.txtid = new System.Windows.Forms.TextBox();
@@ -47,8 +48,10 @@ namespace Finalproject
             this.btn_CreateStaff = new System.Windows.Forms.Button();
             this.btn_back = new System.Windows.Forms.Button();
             this.tlpId = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -68,9 +71,9 @@ namespace Finalproject
             // 
             this.lblid.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lblid.AutoSize = true;
-            this.lblid.Location = new System.Drawing.Point(3, 42);
+            this.lblid.Location = new System.Drawing.Point(3, 40);
             this.lblid.Name = "lblid";
-            this.lblid.Size = new System.Drawing.Size(54, 15);
+            this.lblid.Size = new System.Drawing.Size(54, 20);
             this.lblid.TabIndex = 0;
             this.lblid.Text = "ID";
             // 
@@ -80,11 +83,12 @@ namespace Finalproject
             this.txtid.Location = new System.Drawing.Point(63, 38);
             this.txtid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtid.Name = "txtid";
-            this.txtid.Size = new System.Drawing.Size(134, 23);
+            this.txtid.Size = new System.Drawing.Size(134, 27);
             this.txtid.TabIndex = 13;
             // 
             // tableLayoutPanel2
             // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
@@ -98,7 +102,10 @@ namespace Finalproject
             this.tableLayoutPanel2.Controls.Add(this.txt_Staff_name, 1, 1);
             this.tableLayoutPanel2.Controls.Add(this.lbl_Staff_type, 0, 4);
             this.tableLayoutPanel2.Controls.Add(this.cmb_StaffType, 1, 4);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(41, 58);
+            this.tableLayoutPanel2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tableLayoutPanel2.ForeColor = System.Drawing.Color.White;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(37, 93);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 5;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
@@ -106,92 +113,88 @@ namespace Finalproject
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(248, 217);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(307, 289);
             this.tableLayoutPanel2.TabIndex = 15;
             // 
             // lbl_Staff_id
             // 
             this.lbl_Staff_id.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Staff_id.AutoSize = true;
-            this.lbl_Staff_id.Location = new System.Drawing.Point(3, 14);
+            this.lbl_Staff_id.Location = new System.Drawing.Point(3, 17);
             this.lbl_Staff_id.Name = "lbl_Staff_id";
-            this.lbl_Staff_id.Size = new System.Drawing.Size(93, 15);
+            this.lbl_Staff_id.Size = new System.Drawing.Size(116, 23);
             this.lbl_Staff_id.TabIndex = 0;
             this.lbl_Staff_id.Text = "Identificador:";
             // 
             // txt_Staff_id
             // 
             this.txt_Staff_id.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Staff_id.Location = new System.Drawing.Point(102, 10);
-            this.txt_Staff_id.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Staff_id.Location = new System.Drawing.Point(125, 13);
             this.txt_Staff_id.Name = "txt_Staff_id";
-            this.txt_Staff_id.Size = new System.Drawing.Size(143, 23);
+            this.txt_Staff_id.Size = new System.Drawing.Size(179, 30);
             this.txt_Staff_id.TabIndex = 13;
             // 
             // lbl_Staff_name
             // 
             this.lbl_Staff_name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Staff_name.AutoSize = true;
-            this.lbl_Staff_name.Location = new System.Drawing.Point(3, 49);
+            this.lbl_Staff_name.Location = new System.Drawing.Point(3, 62);
             this.lbl_Staff_name.Name = "lbl_Staff_name";
-            this.lbl_Staff_name.Size = new System.Drawing.Size(93, 30);
+            this.lbl_Staff_name.Size = new System.Drawing.Size(116, 46);
             this.lbl_Staff_name.TabIndex = 1;
             this.lbl_Staff_name.Text = "Nombre completo:";
             // 
             // txt_Staff_Address
             // 
             this.txt_Staff_Address.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Staff_Address.Location = new System.Drawing.Point(102, 139);
-            this.txt_Staff_Address.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Staff_Address.Location = new System.Drawing.Point(125, 184);
             this.txt_Staff_Address.Name = "txt_Staff_Address";
-            this.txt_Staff_Address.Size = new System.Drawing.Size(143, 23);
+            this.txt_Staff_Address.Size = new System.Drawing.Size(179, 30);
             this.txt_Staff_Address.TabIndex = 9;
             // 
             // lbl_Staff_email
             // 
             this.lbl_Staff_email.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Staff_email.AutoSize = true;
-            this.lbl_Staff_email.Location = new System.Drawing.Point(3, 100);
+            this.lbl_Staff_email.Location = new System.Drawing.Point(3, 131);
             this.lbl_Staff_email.Name = "lbl_Staff_email";
-            this.lbl_Staff_email.Size = new System.Drawing.Size(93, 15);
+            this.lbl_Staff_email.Size = new System.Drawing.Size(116, 23);
             this.lbl_Staff_email.TabIndex = 2;
             this.lbl_Staff_email.Text = "Email:";
             // 
             // txt_Staff_Email
             // 
             this.txt_Staff_Email.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Staff_Email.Location = new System.Drawing.Point(102, 96);
-            this.txt_Staff_Email.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Staff_Email.Location = new System.Drawing.Point(125, 127);
             this.txt_Staff_Email.Name = "txt_Staff_Email";
-            this.txt_Staff_Email.Size = new System.Drawing.Size(143, 23);
+            this.txt_Staff_Email.Size = new System.Drawing.Size(179, 30);
             this.txt_Staff_Email.TabIndex = 8;
             // 
             // lbl_Staff_address
             // 
             this.lbl_Staff_address.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Staff_address.AutoSize = true;
-            this.lbl_Staff_address.Location = new System.Drawing.Point(3, 143);
+            this.lbl_Staff_address.Location = new System.Drawing.Point(3, 188);
             this.lbl_Staff_address.Name = "lbl_Staff_address";
-            this.lbl_Staff_address.Size = new System.Drawing.Size(93, 15);
+            this.lbl_Staff_address.Size = new System.Drawing.Size(116, 23);
             this.lbl_Staff_address.TabIndex = 3;
             this.lbl_Staff_address.Text = "Dirección:";
             // 
             // txt_Staff_name
             // 
             this.txt_Staff_name.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_Staff_name.Location = new System.Drawing.Point(102, 53);
-            this.txt_Staff_name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Staff_name.Location = new System.Drawing.Point(125, 70);
             this.txt_Staff_name.Name = "txt_Staff_name";
-            this.txt_Staff_name.Size = new System.Drawing.Size(143, 23);
+            this.txt_Staff_name.Size = new System.Drawing.Size(179, 30);
             this.txt_Staff_name.TabIndex = 7;
             // 
             // lbl_Staff_type
             // 
             this.lbl_Staff_type.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Staff_type.AutoSize = true;
-            this.lbl_Staff_type.Location = new System.Drawing.Point(3, 179);
+            this.lbl_Staff_type.Location = new System.Drawing.Point(3, 235);
             this.lbl_Staff_type.Name = "lbl_Staff_type";
-            this.lbl_Staff_type.Size = new System.Drawing.Size(93, 30);
+            this.lbl_Staff_type.Size = new System.Drawing.Size(116, 46);
             this.lbl_Staff_type.TabIndex = 5;
             this.lbl_Staff_type.Text = "Tipo de empleado:";
             // 
@@ -203,29 +206,40 @@ namespace Finalproject
             this.cmb_StaffType.Items.AddRange(new object[] {
             "Cabin Manager",
             "Staff"});
-            this.cmb_StaffType.Location = new System.Drawing.Point(102, 183);
+            this.cmb_StaffType.Location = new System.Drawing.Point(125, 244);
+            this.cmb_StaffType.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmb_StaffType.Name = "cmb_StaffType";
-            this.cmb_StaffType.Size = new System.Drawing.Size(143, 23);
+            this.cmb_StaffType.Size = new System.Drawing.Size(179, 31);
             this.cmb_StaffType.TabIndex = 14;
             // 
             // btn_CreateStaff
             // 
-            this.btn_CreateStaff.Location = new System.Drawing.Point(193, 303);
+            this.btn_CreateStaff.BackColor = System.Drawing.Color.White;
+            this.btn_CreateStaff.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btn_CreateStaff.FlatAppearance.BorderSize = 3;
+            this.btn_CreateStaff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_CreateStaff.Location = new System.Drawing.Point(221, 404);
+            this.btn_CreateStaff.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_CreateStaff.Name = "btn_CreateStaff";
-            this.btn_CreateStaff.Size = new System.Drawing.Size(93, 37);
+            this.btn_CreateStaff.Size = new System.Drawing.Size(106, 49);
             this.btn_CreateStaff.TabIndex = 16;
             this.btn_CreateStaff.Text = "Registrar";
-            this.btn_CreateStaff.UseVisualStyleBackColor = true;
+            this.btn_CreateStaff.UseVisualStyleBackColor = false;
             this.btn_CreateStaff.Click += new System.EventHandler(this.btn_CreateStaff_Click);
             // 
             // btn_back
             // 
-            this.btn_back.Location = new System.Drawing.Point(41, 303);
+            this.btn_back.BackColor = System.Drawing.Color.White;
+            this.btn_back.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btn_back.FlatAppearance.BorderSize = 3;
+            this.btn_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_back.Location = new System.Drawing.Point(47, 404);
+            this.btn_back.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_back.Name = "btn_back";
-            this.btn_back.Size = new System.Drawing.Size(93, 37);
+            this.btn_back.Size = new System.Drawing.Size(106, 49);
             this.btn_back.TabIndex = 17;
             this.btn_back.Text = "Atrás";
-            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.UseVisualStyleBackColor = false;
             this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
             // 
             // tlpId
@@ -234,24 +248,42 @@ namespace Finalproject
             this.tlpId.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
             this.tlpId.ToolTipTitle = "Tipo de empleado";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Finalproject.Properties.Resources._4f60305f48b01e05db6b53083abf71dd1;
+            this.pictureBox1.Location = new System.Drawing.Point(123, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 74);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
             // frm_NewStaff
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 352);
+            this.BackgroundImage = global::Finalproject.Properties.Resources._0kTnyvL;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(383, 469);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_back);
             this.Controls.Add(this.btn_CreateStaff);
             this.Controls.Add(this.tableLayoutPanel2);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.Name = "frm_NewStaff";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormNewStaff";
+            this.Text = "NUEVO USUARIO";
             this.Load += new System.EventHandler(this.frm_NewStaff_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -275,5 +307,6 @@ namespace Finalproject
         private System.Windows.Forms.Button btn_CreateStaff;
         private System.Windows.Forms.Button btn_back;
         private System.Windows.Forms.ToolTip tlpId;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
